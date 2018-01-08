5f654a5fe2315f028f90296e030d140c858235f1
nick554 <xiaokong.chen@gmail.com>
2018-01-08 20:09:35 +0800

    get sigmoid gradient

--- MachineLearning-ByAndrewNg/machine-learning-ex4/sigmoidGradient.m
+++ MachineLearning-ByAndrewNg/machine-learning-ex4/sigmoidGradient.m
@@ -12,18 +12,9 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
+tmp = sigmoid(z);
 
-
-
-
-
-
-
-
-
-
-
-
+g = tmp .* (1 - tmp);
 
 % =============================================================
 
