acbb6f20e25bfa982f36ce753fb460fa80472d22
nick554 <xiaokong.chen@gmail.com>
2018-01-14 22:31:08 +0800

    find best Parameter

--- MachineLearning-ByAndrewNg/machine-learning-ex6/dataset3Params.m
+++ MachineLearning-ByAndrewNg/machine-learning-ex6/dataset3Params.m
@@ -9,7 +9,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +23,19 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+error_val = zeros(8,8);
+list = [0.01 0.03 0.1 0.3 1 3 10 30];
 
-
-
-
-
+for i = 1%:8
+    for j = 1%:8
+        % calculate the model
+        model = svmTrain(X,y,list(i), @(x1,x2)gaussianKernel( x1, x2, list(j)));
+        % find out the error
+        predictions = svmPredict(model, Xval);
+        % update the best solution
+        error_val(i,j) = mean(double(predictions ~= yval));
+    end
+end
 
 % =========================================================================
 
